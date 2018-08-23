eed5309955daa4fbace328cbd49ab232869d56b7
Morgan Newman <mpnewman95@gmail.com>
2018-08-23 12:20:19 -0700

    Added append functionality to the shopping list

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 'use strict';
+
+const generateListItemHTML = function(userInput) {
+  return `
+    <li>
+    <span class="shopping-item">${userInput}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>
+  `;
+};
+
 $(function(){
   $('#js-shopping-list-form').on('submit', function(event){
     event.preventDefault();
     let userInputField = $('.js-shopping-list-entry');
     let userInput = userInputField.val();
     userInputField.val('');
-
-  })
+    
+    // Add our HTML to the DOM
+    $('.shopping-list').append(generateListItemHTML(userInput));
+  });
 });
