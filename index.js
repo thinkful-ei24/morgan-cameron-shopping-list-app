01664bb3b3928eedfb67ef21860a4c65e61884b0
Morgan Newman <mpnewman95@gmail.com>
2018-08-23 12:46:02 -0700

    Completed the shopping list challenge!

--- index.js
+++ index.js
@@ -16,18 +16,26 @@ const generateListItemHTML = function(userInput) {
   `;
 };
 
-$(function(){
-  $('#js-shopping-list-form').on('submit', function(event){
+$(function() {
+  // Append items to the list
+  $('#js-shopping-list-form').on('submit', function(event) {
     event.preventDefault();
     let userInputField = $('.js-shopping-list-entry');
     let userInput = userInputField.val();
     userInputField.val('');
-    
+
     // Add our HTML to the DOM
     $('.shopping-list').append(generateListItemHTML(userInput));
-
   });
-  $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+
+  // Toggling the checked state of shopping items
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
     $(this).parent().prev('.shopping-item').toggleClass('shopping-item__checked');
   });
+
+  // Remove items on button press
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(event.currentTarget).parents('li').remove();
+  });
+
 });
